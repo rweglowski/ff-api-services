89c20b9c4023f9dce3e917c0879fdd77eae977bb
Nico Miebach <nico@miebach.one>
2021-06-11 11:06:11 +0200

    Update src/service/HistoryModuleService/UserHistoryController.ts
    Co-authored-by: Jan Höck <jan.hoeck@flowfact.de>

--- src/service/HistoryModuleService/UserHistoryController.ts
+++ src/service/HistoryModuleService/UserHistoryController.ts
@@ -17,7 +17,7 @@ export default class UserHistoryController extends APIClient {
      * @param userIds
      * @param filter
      */
-    async fetchUserHistoryActivity(size: number = 100, offset: number = 0, sourceTypes: SourceType[] = ['ALL'], userIds: string[], filter?: HistoryFilter): Promise<ApiResponse<UserHistory>> {
+    async fetchUserHistoryActivity(size: number = 100, offset: number = 0, sourceTypes: SourceType[] = ['ALL'], userIds: string[], filter?: HistoryFilter) {
         let body: any = {
             size,
             offset,
