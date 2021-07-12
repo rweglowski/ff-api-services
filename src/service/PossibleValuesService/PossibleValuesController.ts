32d0b8c037a8f4607fbb70bbba90198524fe5e0d
Richard Hoffmann <richard.hoffmann@flowfact.de>
2021-07-12 15:54:00 +0200

    using header v2

--- src/service/PossibleValuesService/PossibleValuesController.ts
+++ src/service/PossibleValuesService/PossibleValuesController.ts
@@ -28,6 +28,9 @@ export default class PossibleValuesController extends APIClient {
     async deletePossibleValue(schemaName: string, fieldName: string, possibleValue: string) {
         return this.invokeApiWithErrorHandling<void>(`/schemas/${schemaName}/fields/${fieldName}/possiblevalues`, 'DELETE', undefined, {
             queryParams: { value: possibleValue },
+            headers: {
+                'x-ff-version': 2,
+            },
         });
     }
 }
