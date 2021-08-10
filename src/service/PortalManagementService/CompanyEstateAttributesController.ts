f2a7859df20289234f2d54b814dc95fb16552b58
istoljarow <igor.stoljarow@flowfact.de>
2021-08-10 16:02:42 +0200

    feat FLOW-14251: [portal-mgm-srv] added new resources for company estate attributes

---
+++ src/service/PortalManagementService/CompanyEstateAttributesController.ts
@@ -0,0 +1,63 @@
+import {APIClient, APIMapping} from "../../http";
+
+/**
+ * Manages company wide attributes for an estate in a Record<string, any> model.
+ */
+export class CompanyEstateAttributesController extends APIClient {
+    constructor() {
+        super(APIMapping.portalManagementService);
+    }
+
+    /**
+     * Setts attributes of given key and value.
+     * @param entityId as UUID
+     * @param key as string
+     * @param value as any - you can set anything your business case needs
+     */
+    async set(entityId: string, key: string, value: any) {
+        return await this.invokeApiWithErrorHandling<Record<string, any>>(`companies/estates/${entityId}/attributes/${key}`, 'POST', value);
+    }
+
+    /**
+     * Setts attributes of given key and value.
+     * @param entityId as UUID
+     * @param attributes as Record containing key: value enties - you can set anything your business case needs
+     */
+    async setAll(entityId: string, attributes: Record<string, any>) {
+        return await this.invokeApiWithErrorHandling<Record<string, any>>(`companies/estates/${entityId}/attributes`, 'POST', attributes);
+    }
+
+    /**
+     * Retrieves a Record containing given attribute by key, if exists.
+     * @param entityId as UUID
+     * @param key as string
+     */
+    async get(entityId: string, key: string) {
+        return await this.invokeApiWithErrorHandling<Record<string, any>>(`companies/estates/${entityId}/attributes/${key}`, 'GET');
+    }
+
+    /**
+     * Retrieves all attributes as Record for given entity
+     * @param entityId as UUID
+     */
+    async getAll(entityId: string) {
+        return await this.invokeApiWithErrorHandling<Record<string, any>>(`companies/estates/${entityId}/attributes`, 'GET');
+    }
+
+    /**
+     * Removes an attribute by providing the key
+     * @param entityId as UUID
+     * @param key as String
+     */
+    async remove(entityId: string, key: string) {
+        return await this.invokeApiWithErrorHandling<Record<string, any>>(`companies/estates/${entityId}/attributes/${key}`, 'DELETE');
+    }
+
+    /**
+     * Removes all attributes from given entity
+     * @param entityId as UUID
+     */
+    async removeAll(entityId: string) {
+        return await this.invokeApiWithErrorHandling<Record<string, any>>(`companies/estates/${entityId}/attributes`, 'DELETE');
+    }
+}
