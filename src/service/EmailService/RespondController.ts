df08462dee244c4913b8ee8d9dac4260e050a589
dpotera <dominik.potera@gmail.com>
2021-08-26 14:19:36 +0200

    FLOW-14801 add return type to email respond controller calls

--- src/service/EmailService/RespondController.ts
+++ src/service/EmailService/RespondController.ts
@@ -11,6 +11,7 @@ export class RespondController extends APIClient {
      *      The id of a entity (schema email)
      * @param replyEmailBody
      *      Body of reply email.
+     * @returns ID of created draft_email
      */
     async replyMail(emailEntityId: string, replyEmailBody: string) {
         return this.replyMailCall(emailEntityId, replyEmailBody, false);
@@ -22,13 +23,14 @@ export class RespondController extends APIClient {
      *      The id of a entity (schema email)
      * @param replyEmailBody
      *      Body of reply email.
+     * @returns ID of created draft_email
      */
     async replyAllMail(emailEntityId: string, replyEmailBody: string) {
         return this.replyMailCall(emailEntityId, replyEmailBody, true);
     }
 
     private async replyMailCall(emailEntityId: string, replyEmailBody: string, replyAll: boolean) {
-        return this.invokeApiWithErrorHandling(`/emails/reply`, 'POST', {
+        return this.invokeApiWithErrorHandling<string>(`/emails/reply`, 'POST', {
             emailEntityId,
             replyEmailBody,
             replyAll,
@@ -43,9 +45,10 @@ export class RespondController extends APIClient {
      * Prepares forward draft_email entity.
      * @param emailEntityId
      *      The id of a entity (schema email)
+     * @returns ID of created draft_email
      */
     async forwardMail(emailEntityId: string) {
-        return this.invokeApiWithErrorHandling(`/emails/forward/${emailEntityId}`, 'POST', undefined, {
+        return this.invokeApiWithErrorHandling<string>(`/emails/forward/${emailEntityId}`, 'POST', undefined, {
             headers: {
                 'x-ff-version': 2,
             }
