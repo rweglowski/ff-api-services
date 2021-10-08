62e6ab90853fe0a97a4ec16529cff0538afbb724
Bartosz Zagozda <52490842+bzagozda-vl@users.noreply.github.com>
2021-10-08 10:20:08 +0200

    FLOW-15589 - [Fe] Check status of import using rest instead websockets (#318)
    * [Fe] Check status of import using rest instead websockets
    
    * FLOW-15589 - [Fe] Check status of import using rest instead websockets
    
    * FLOW-15589 [Fe] Check status of import using rest instead websockets
    
    * FLOW-15589 - [Fe] Check status of import using rest instead websockets

--- src/service/CsvDataToEntityImporterService/CsvDataToEntityImporter.Types.ts
+++ src/service/CsvDataToEntityImporterService/CsvDataToEntityImporter.Types.ts
@@ -1,20 +1,30 @@
-import { SchemaV2FieldType } from "@flowfact/types";
+import { SchemaV2FieldType } from '@flowfact/types';
 
 export namespace CsvDataToEntityImporter {
-  export interface ImportStatus {
-      entries: number,
-      imported: number,
-      failed: number,
-  }
-  
-  export interface ImportField {
-    name: string
-    value: string
-    type: SchemaV2FieldType
-    caption: string
-    condition: string
-    maxOccurs: number
-    possibleValues: Array<ImportField>
-    selectedValues: Array<ImportField>
-}
+    export interface ImportStatus {
+        entries: number;
+        imported: number;
+        failed: number;
+    }
+
+    export interface ImportField {
+        name: string;
+        value?: string;
+        type?: SchemaV2FieldType;
+        caption?: string;
+        condition?: string;
+        hasMultipleValues?: boolean;
+        possibleValues: Array<ImportField>;
+        selectedValues: Array<ImportField>;
+    }
+
+    export interface ImportMapping {
+        fileId: string;
+        delimiter: string;
+        targetSchema: string;
+        schemaFields: ImportField[];
+        mapping: {
+            [fieldName: string]: ImportField;
+        };
+    }
 }
