1acfc38766428df5aa2092d41f293eb29df76ee2
Bartosz Zagozda <52490842+bzagozda-vl@users.noreply.github.com>
2022-01-17 11:44:02 +0100

    Change importer record log to use lambda (#393)
    * Change importer record log to use lambda
    
    * Change body to undefined

--- src/service/ImporterRecordLogService/ImporterRecordLogController.ts
+++ src/service/ImporterRecordLogService/ImporterRecordLogController.ts
@@ -2,14 +2,18 @@ import { APIClient, APIMapping } from '../../http';
 
 export class ImporterRecordLogController extends APIClient {
     constructor() {
-        super(APIMapping.lambdaProxyService);
+        super(APIMapping.flowfactImporterLambda);
     }
 
     async fetchLogDirectories() {
-        return this.invokeApiWithErrorHandling('/ff-importer-v2-record-log-s3-directory-entries', 'GET');
+        return this.invokeApiWithErrorHandling('/record-log/directory/content', 'GET');
     }
 
     async fetchDirectoryContents(table: string) {
-        return this.invokeApiWithErrorHandling('/ff-importer-v2-record-log-s3-directory-content', 'POST', { table });
+        return this.invokeApiWithErrorHandling(`/record-log/directory/entries`, 'GET', undefined, {
+            queryParams: {
+                table,
+            },
+        });
     }
 }
