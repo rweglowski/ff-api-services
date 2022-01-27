3449114548ea0b7788660bb123b3efb1a437117f
Aaron Weigl <85172474+aaronWeigl@users.noreply.github.com>
2022-01-27 17:20:27 +0100

    add restoreButton (#408)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,26 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true,
-        'node': true,
-        'jest': true,
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+        jest: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'project': './tsconfig.json',
-        'sourceType': 'module',
+    extends: ['eslint:recommended', 'plugin:import/errors', 'plugin:import/warnings', 'plugin:import/typescript'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
     },
-    'plugins': [
-        '@typescript-eslint',
-        'import',
-    ],
-    'rules': {
+    plugins: ['@typescript-eslint', 'import'],
+    rules: {
         'no-setter-return': 'error',
-        'yoda': 'error',
-        'no-unused-vars': 'warn',
+        yoda: 'error',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
         'import/no-internal-modules': [
             'warn',
             {
-                'allow': [
-                    '**/actions/*',
-                    'source-map-support/*',
-                ],
+                allow: ['**/actions/*', 'source-map-support/*'],
             },
         ],
     },
