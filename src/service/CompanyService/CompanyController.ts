af53ee824bcc3afdd33a102c8427526d9e47ca34
dpotera <dominik.potera@gmail.com>
2022-03-17 13:23:51 +0100

    FLOW-20373 move company metadata to separate controller

--- src/service/CompanyService/CompanyController.ts
+++ src/service/CompanyService/CompanyController.ts
@@ -115,20 +115,4 @@ export class CompanyController extends APIClient {
         return this.invokeApiWithErrorHandling(`/company/${companyId}/ownerclass/${ownerClass}`, 'PUT');
     }
 
-
-    /**
-     * Loads metadata for company
-     */
-    async fetchCompanyMetadata() {
-        return this.invokeApiWithErrorHandling<{ entries: CompanyServiceTypes.CompanyMetadata<any>[] }>('/metadata', 'GET');
-    }
-
-    /**
-     * Save metadata for company
-     * creates new metadata or updates existing one
-     */
-    async saveCompanyMetadata(metadata: CompanyServiceTypes.CompanyMetadata<any>) {
-        return this.invokeApiWithErrorHandling<CompanyServiceTypes.CompanyMetadata<any>>('/metadata', 'POST', metadata);
-    }
-
 }
--- src/service/CompanyService/index.ts
+++ src/service/CompanyService/index.ts
@@ -1,6 +1,7 @@
 import { CompanyController } from './CompanyController';
 import { InternalController } from './InternalController';
 import { LegislationTextController } from './LegislationTextController';
+import { MetadataController } from './MetadataController';
 
 export * from './CompanyService.Types';
 
@@ -10,11 +11,13 @@ export class CompanyService {
     public readonly company: CompanyController;
     public readonly internal: InternalController;
     public readonly legislationText: LegislationTextController;
+    public readonly metadata: MetadataController;
 
     constructor() {
         this.company = new CompanyController();
         this.internal = new InternalController();
         this.legislationText = new LegislationTextController();
+        this.metadata = new MetadataController();
     }
 }
 
