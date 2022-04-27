3694e0c93c7f40b2e2f1996c5015d9e8268273bd
tohoffmann <tobias.hoffmann@flowfatc.de>
2022-04-27 13:58:03 +0200

    FLOW-20858: Add acp endpoints

--- src/service/AclGroupService/index.ts
+++ src/service/AclGroupService/index.ts
@@ -1,13 +1,16 @@
 import { AclGroupController } from './AclGroupController';
 import { AcpTemplateController } from './AcpTemplateController';
+import {AcpDefaultAssignmentsController} from "./AcpDefaultAssignmentsController";
 
 export class AclGroupService {
     public readonly aclGroupService: AclGroupController;
     public readonly acpTemplateService: AcpTemplateController;
+    public readonly acpDefaultAssignment: AcpDefaultAssignmentsController
 
     constructor() {
         this.aclGroupService = new AclGroupController();
         this.acpTemplateService = new AcpTemplateController();
+        this.acpDefaultAssignment = new AcpDefaultAssignmentsController()
     }
 }
 
